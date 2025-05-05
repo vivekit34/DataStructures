66875eda3347d2ee3d0fafa8ff907e4036ba8a33
vivekit34 <vivekit34@gmail.com>
2025-05-05 07:38:05 +0530

    test

--- src/main/java/ArraysAndHashing/ContainsDuplicate.java
+++ src/main/java/ArraysAndHashing/ContainsDuplicate.java
@@ -4,7 +4,10 @@ import java.util.HashMap;
 
 //1.Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
 public class ContainsDuplicate {
-    public boolean hasDuplicate(int[] nums) {
+    public ContainsDuplicate() {
+    }
+
+    public static boolean hasDuplicate(int[] nums) {
         HashMap<Integer, Integer> hm= new HashMap<>();
         for (int num : nums) {
             if (hm.get(num) != null) {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import ArraysAndHashing.ContainsDuplicate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Application started...");
+
+//        int[] a={2,1, 6, 8, 9,10, 3, 2, 1, 1, 5, 5 ,5,5};
+//        int[] b={2,1, 6};
+//        System.out.println("Returned vales for a: "+ ContainsDuplicate.hasDuplicate(a));
+//        System.out.println("Returned vales for b: "+ ContainsDuplicate.hasDuplicate(b));
+
+
+        System.out.println("Application ended...");
     }
 }
